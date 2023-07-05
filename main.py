b63bb3b598753c7e95b5096a2d3134531ba58c1a
Puspabeethi Samanta <117652427+puspabeethi@users.noreply.github.com>
2023-07-05 14:43:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ import DNA_Random_Access_Encoder as ra
 begin_time = datetime.datetime.now()
 
 dna.encoder("test.txt", 200)
-ra.encoder("C://Users//User//Desktop//Primer and Codebook design//9.Python_module//test_folder", 200)
 
 print(datetime.datetime.now() - begin_time)
